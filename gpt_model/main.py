497b48a9a10235a50a506124c419f8ff784a7489
Chan-py <unknown@unist.ac.kr>
2024-09-11 00:13:23 +0900

    change to gpt4 and instruction changed

--- gpt_model/main.py
+++ gpt_model/main.py
@@ -15,9 +15,10 @@ def detect():
     data = request.json
     
     message = data["message"]
+    message = message.replace('\n', '').strip()
     type = data["type"]
     
-    prompt = f"{message}\n이 문장에서 {type}가 포함되어 있다면 그 부분을 마스킹해줘."
+    prompt = f"{message}"
     print("Received Prompt:", prompt)
     
     result = gpt(prompt)
@@ -36,12 +37,14 @@ def gpt(prompt):
                 "content": (
                     "You are an expert assistant that helps to filter offensive words or harmful, illegal, and scam websites in the sentence. "
                     "There are two tasks you have to do."
-                    "First, mask the offensive words with '***'. Consider the context while determining whether there exists offensive words or not."
-                    "If a word could be harmful or offensive in a certain context, but not in another, make sure to only mask it if it is harmful or offensive in the given context."
+                    "First, mask the offensive words with '***'. Do not classify a word as offensive if the sentence does not convey an aggressive or hostile tone, considering the context."
+                    "If you're not certain whether a word contains offensive language, assume that it does not."
                     "Second, mask harmful, illegal, and scam websites with '*****'. You might use scam advisor to check whether it is scam site or not."
-                    "If the sentence or URL is clean and contains no offensive language or harmful content, just reply with the original sentence. "
-                    "'이 문장에서 비속어 또는 유해사이트가 포함되어 있다면 그 부분을 마스킹해줘.' This part is just the command. It might not be included in the output."
-                    "Do not add any other words in the output. It must be organized only with original sentence or '*'."
+                    "If the sentence or URL is clean and contains no offensive language or harmful content, just reply with the original input sentence."
+                    "Do not add any other words in the output."
+                    "When masking harmful websites, mask the entire URL."
+                    "When masking offensive language, if a phrase like '엄마 없냐' is combined to form a single offensive expression, recognize it as one offensive word and mask it as '***'."
+                    "Response has to be consistent, for the same input."
                 )
             },
             {
@@ -49,7 +52,8 @@ def gpt(prompt):
                 "content": prompt
             }
         ],
-        model="ft:gpt-4o-mini-2024-07-18:personal:chat-filtering:A5uOxEEa",
+        model="gpt-4",
+        #model="ft:gpt-4o-mini-2024-07-18:personal:chat-filtering:A5vtEEgW",
         max_tokens=500
     )
     
