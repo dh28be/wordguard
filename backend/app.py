14f535e7e56309b71fb8a1f8c8f9ebdf32097c53
dh28be <dh28be@gmail.com>
2024-09-11 08:17:54 +0900

    CORS

--- backend/app.py
+++ backend/app.py
@@ -208,4 +208,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.secret_key = "super secret key"
-    socketio.run(app, host="0.0.0.0", port=80, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=80, debug=False, allow_unsafe_werkzeug=True)
